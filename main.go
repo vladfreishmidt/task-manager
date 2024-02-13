e4d7fc58cce39fa1ad24381858b4d135c58ba139
vladfreishmidt <vladfreishmidt.webdevelopment@gmail.com>
2024-02-13 11:53:30 +0200

    Update projectView handler to accept id query string parameter

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -15,7 +17,13 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func projectView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific project..."))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Display a specific project with ID %d...", id)
 }
 
 func projectCreate(w http.ResponseWriter, r *http.Request) {
