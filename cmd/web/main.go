3a38cf847ec45731d7717bb18c0bdb91cbf1869e
vladfreishmidt <vladfreishmidt.webdevelopment@gmail.com>
2024-02-16 11:50:39 +0200

    feat: add leveled logging capability breaking apart log messages into "INFO" and "ERROR" levels

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,23 +4,32 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	// command-line flags
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
 
+	// loggers
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// router
 	mux := http.NewServeMux()
 
+	// file server
 	fileServer := http.FileServer(http.Dir("./ui/static"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
+	// routes
 	mux.HandleFunc("/", dashboard)
 	mux.HandleFunc("/projects", projectListView)
 	mux.HandleFunc("/project/view", projectView)
 	mux.HandleFunc("/project/create", projectCreate)
 
-	log.Printf("Starting server on %s", *addr)
+	infoLog.Printf("Starting server on %s", *addr)
 	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	errorLog.Fatal(err)
 }
