1088adca6ccb6dbe1bddac4a2b78586ff9ab9346
vladfreishmidt <vladfreishmidt.webdevelopment@gmail.com>
2024-02-23 12:18:34 +0200

    refactoring: replace router with /julienschmidt/httprouter

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,25 +1,32 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
 
-func (app *application) routes() *http.ServeMux {
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) routes() http.Handler {
 	// router
-	mux := http.NewServeMux()
+	router := httprouter.New()
 
 	// file server
-	fileServer := http.FileServer(http.Dir("./ui/static"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
 
 	// routes
 
 	// workspaces
-	mux.HandleFunc("/workspaces", app.workspaceListView)
+	router.HandlerFunc(http.MethodGet, "/workspaces", app.workspaceListView)
+	router.HandlerFunc(http.MethodGet, "/workspaces/:id", app.workspaceView)
+	// router.HandlerFunc(http.MethodGet, "/workspaces/create", app.workspaceCreate)
+	router.HandlerFunc(http.MethodPost, "/workspaces/create", app.workspaceCreatePost)
 
 	// projects
-	mux.HandleFunc("/", app.dashboard)
-	mux.HandleFunc("/projects", app.projectListView)
-	mux.HandleFunc("/project/view", app.projectView)
-	mux.HandleFunc("/project/create", app.projectCreate)
+	// mux.HandleFunc("/", app.dashboard)
+	// mux.HandleFunc("/projects", app.projectListView)
+	// mux.HandleFunc("/project/view", app.projectView)
+	// mux.HandleFunc("/project/create", app.projectCreate)
 
-	return mux
+	return router
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/vladfreishmidt/task-manager
 
 go 1.22.0
 
-require github.com/go-sql-driver/mysql v1.7.1 // indirect
+require (
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
+)
